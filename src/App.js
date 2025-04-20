02d9ff2d2ca59e30ee7b15ec2c05093a7c9f9d95
Boniface <bm715452@gmail.com>
2025-04-20 03:33:06 +0300

    Complete profile component with passing tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Boniface Mwangi</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE3yci50FL8gvlOAJHcFHnHQ3Cvo6FqFPzRV5jxrj95sQiev4puv8jttNtrXnIYNkTe_0&usqp=CAU"     
+        alt="Boniface Mwangi"
+      />
+      <h2>About Me</h2>
+      <p>
+        I'm a software developer passionate about creating web applications.
+        My background includes working with Javascript, React, and json.
+      </p>
+      <div>
+      <a href="https://github.com/bonmwang" target="_blank" rel="noopener noreferrer">Github</a>
+      <a href="https://www.linkedin.com/in/boniface-mwangi-043262218/" target="_blank" rel="noopener noreferrer" style={{marginLeft: '10px'}}>LinkedIn</a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,53 @@
-import { render, screen } from "@testing-library/react";
+import {render, screen } from "@testing-library/react";
+import App from "../App";
 import '@testing-library/jest-dom';
 
-import App from "../App";
+test("displays a top-level heading with text `Hi, I'm Boniface Mwangi`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const topLveleHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+    // Assert
+    expect(topLveleHeading).toBeInTheDocument();
+});
 
-// Your tests here
+test("displays an image with alt text", () => {
+    render(<App/>);
+    const image = screen.getByAltText(/boniface mwangi/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src');
+});
+
+test("displays a second-level heading with text 'About Me'", () => {
+    render(<App/>);
+    const aboutHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        level: 2,
+    });
+    expect(aboutHeading).toBeInTheDocument();
+});
+
+test("displays a paragraph for biography", () => {
+    render(<App/>);
+    const bioParagraph = screen.getByText(/I'm a software developer passionate about creating web applications./i);
+    expect(bioParagraph).toBeInTheDocument();
+    expect(bioParagraph.tagName).toBe('P');
+});
+
+test("displays Github link", () => {
+    render(<App/>);
+    const githubLink = screen.getByRole('link', { name: /github/i });
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute('href', expect.stringContaining('github.com'));
+});
+
+test("displays LinkedIn link", () => {
+    render(<App />);
+    const linkedinLink = screen.getByRole('link', { name: /linkedin/i });
+    expect(linkedinLink).toBeInTheDocument();
+    expect(linkedinLink).toHaveAttribute('href', expect.stringContaining('linkedin.com'));
+})
